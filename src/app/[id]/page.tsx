3a4c9478f08a2718629fc2e8552f7a74fba0e029
youtube-engineer <chatgptinjapan@gmail.com>
2024-12-04 10:34:42 +0000

    初期プロジェクト

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ShareText({ params }: { params: { id: string } }) {
+    console.log(params.id);
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            {params.id}
+        </div>
+    );
+}
