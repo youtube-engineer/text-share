b3b8c3316634bdffb19e271e8b1481a3bb60d8c1
youtube-engineer <chatgptinjapan@gmail.com>
2024-12-15 07:41:40 +0000

    テスト

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,19 +1,26 @@
-import { findSharedText } from "../../actions/text-share"
-export const dynamic = "force-dynamic";
+// import { findSharedText } from "../../actions/text-share"
 export const runtime = "edge";
 
-type Props = {
-    params: Promise<{
-        id: string
-    }>;
-}
-export default async function ShareText({ params }: Props) {
-    const { id } = await params
-    const sharedText = await findSharedText(id)
+// type Props = {
+//     params: Promise<{
+//         id: string
+//     }>;
+// }
+// export default async function ShareText({ params }: Props) {
+//     const { id } = await params
+//     const sharedText = await findSharedText(id)
 
+//     return (
+//         <pre className="p-16">
+//             {sharedText}
+//         </pre>
+//     );
+// }
+
+
+
+export const page = () => {
     return (
-        <pre className="p-16">
-            {sharedText}
-        </pre>
-    );
+        <div>page</div>
+    )
 }
