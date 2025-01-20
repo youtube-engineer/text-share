2d99360599ceb330f7241f585a5253beeb01253a
youtube-engineer <chatgptinjapan@gmail.com>
2025-01-20 10:52:26 +0000

    createSharedTextでprisma使用しない

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -20,7 +20,7 @@ export default function Form() {
                 className="placeholder-gray-300 text-black w-full max-w-2xl p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent mb-4"
                 rows={5}
             />
-            <button onClick={async () => await createSharedText(setData)} className="bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-all duration-200">
+            <button onClick={async () => await createSharedText()} className="bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-all duration-200">
                 シェアする
             </button>
         </div>
