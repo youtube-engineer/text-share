fc406e09b5d6547d3b3b7e1c82b1ef6b57a9d396
youtube-engineer <chatgptinjapan@gmail.com>
2025-04-23 12:46:51 +0000

    upgrade : nodeのバージョンを22.14.0に更新&prisma

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WORKSPACE_DIRECTORY="/workspace"
+
+(
+  cd $WORKSPACE_DIRECTORY
+
+  # Set owner
+  sudo chown -R $(id -un):$(id -gn) .
+  sudo chown $(id -un):$(id -gn) node_modules /opt/pnpm/store /home/vscode/.cache/ /home/vscode/.cache/* /home/vscode/.ssh /home/vscode/.aws
+
+  # Set safe directory
+  git config --global safe.directory $WORKSPACE_DIRECTORY
+
+  # Insall Node.js
+  NVM_DIR="${NVM_DIR:-"/usr/local/share/nvm"}"
+  . $NVM_DIR/nvm.sh
+  nvm install 
+  nvm use
+  node -v
+)
